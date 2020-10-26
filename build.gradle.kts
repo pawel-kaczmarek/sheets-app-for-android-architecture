26b69910dcb4e6e68fee8d93dc1eb9e014d308d9
Paweł Kaczmarek <pawelkaczmarek12@gmail.com>
2020-10-26 18:40:51 +0100

    Added copying jar file to root buildDir

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
 tasks {
     val stage by registering {
         dependsOn(getTasksByName("build", true))
+
+        doLast {
+            copy {
+                from("$rootDir/application/build/libs")
+                into("$buildDir/libs")
+            }
+        }
     }
 }
